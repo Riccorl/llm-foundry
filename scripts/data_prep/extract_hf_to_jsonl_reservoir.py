25247c1c3e105b53b1468a01281e4550405165ee
Andrew-Wyn <terraformer.144@gmail.com>
2024-01-23 09:56:20 +0100

    reservoir sampling, saving urls, fixed reservoir bugs

--- scripts/data_prep/extract_hf_to_jsonl_reservoir.py
+++ scripts/data_prep/extract_hf_to_jsonl_reservoir.py
@@ -3,51 +3,46 @@ import json
 import logging
 import os
 from pathlib import Path
-
-import huggingface_hub
 import psutil
-from datasets import Dataset, load_dataset, DatasetDict
-from huggingface_hub import login
+from datasets import load_dataset
 from tqdm import tqdm
 from random import randrange
+from pathlib import Path
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 HF_CACHE = os.getenv("HF_HOME", os.path.expanduser("~/.cache/huggingface"))
-print(HF_CACHE)
+URLS_FILE_NAME = "urls.json"
 
 
-def reservoir_streaming(dataset, max_samples, sources, ds_split="train"):
+def reservoir_streaming(dataset, max_samples, sources, url_file_path):
     reservoir_indexes = None
 
     reservoir_indexes = []
 
-    if sources is None: # if the sources is None, we can loop over a fake range
-        for i in tqdm(range(dataset[ds_split].dataset_size)):
-            
-            # init the reservoir
-            if len(reservoir_indexes) < max_samples:
-                reservoir_indexes.append(i)
-                continue
-            
-            # reservoir sample
-            j = randrange(i)
-            reservoir_indexes[j] = i
-    else:
-        for i, sample in tqdm(enumerate(dataset[ds_split])):
+    with open(url_file_path, "w") as url_file:
+
+        for i, sample in tqdm(enumerate(dataset)):
+
+            # save the url 
+            url_file.write(json.dumps({"index": i, "url": sample["url"]}) + "\n")
+
             # sample only certain resources
             if sources is not None and sample["source"] not in sources:
                 continue
             
-            # init the reservoir
+            # --- init the reservoir
             if len(reservoir_indexes) < max_samples:
                 reservoir_indexes.append(i)
                 continue
+            # ---
             
-            # reservoir sample
+            # --- reservoir sample
             j = randrange(i)
-            reservoir_indexes[j] = i
+            if j < max_samples:
+                reservoir_indexes[j] = i
+            # ---
 
     # return a uniform sample of indexes
     return reservoir_indexes
@@ -87,25 +82,32 @@ def save_to_jsonl(dataset, path_to_save, split_size, max_samples, reservoir_inde
 
 
 def main(args):
-    token = huggingface_hub.HfFolder.get_token()
-    if token is None:
-        logger.info("HuggingFace Login")
-        login()
+    #token = huggingface_hub.HfFolder.get_token()
+    #if token is None:
+    #    logger.info("HuggingFace Login")
+    #    login()
 
-    logger.info("==== Starting download CulturaX ====")
+    logger.info("==== Starting download Dataset ====")
     dataset = load_dataset(
         path=args.dataset_path,
-        # split="train",
+        split=args.ds_split,
         cache_dir=HF_CACHE,
-        token=True,
+    #    token=True,
         num_proc=psutil.cpu_count(logical=False),
     )
 
     # sources = ["mC4", "OSCAR-2301", "OSCAR-2201"]
 
-    reservoir_indexes  = reservoir_streaming(dataset, args.max_samples, args.sources, ds_split="train")
+    # create path for the URLs file
 
-    save_to_jsonl(dataset, args.path_to_save, args.split_size, args.max_samples, reservoir_indexes)
+    if not os.path.exists(args.url_file_path):
+        os.makedirs(args.url_file_path)
+
+    url_file_path = Path(args.url_file_path) / URLS_FILE_NAME
+
+    reservoir_indexes  = reservoir_streaming(dataset, args.max_samples, args.sources, url_file_path)
+
+    save_to_jsonl(dataset, args.path_to_save, args.split_size, args.max_samples, reservoir_indexes, args.ds_split)
 
 
 if __name__ == "__main__":
@@ -145,6 +147,12 @@ if __name__ == "__main__":
         type=str,
         default="train",
         help="dataset split to retrieve"
+    )
+    parser.add_argument(
+        "--url_file_path",
+        type=str,
+        default="",
+        help="path to save the url file"
     )
     args = parser.parse_args()
 
